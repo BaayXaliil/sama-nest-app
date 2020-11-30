0505c1766bec7b4459727fc78ccb0a75c4777296
BaayXaliil <dioptrie7@gmail.com>
2020-11-30 05:05:43 +0000

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { HelmetMiddleware } from '@nest-middlewares/helmet';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { UserModule } from './user/user.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ProductsModule,
+    UserModule,
+    MongooseModule.forRoot('mongodb+srv://baayxaliil:daldiam7@cluster0.gchcm.mongodb.net/nest-test?retryWrites=true&w=majority')
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(HelmetMiddleware)
+      .forRoutes('');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv'
+import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
-dotenv.config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  app.use(morgan('dev'))
+  app.enableCors()
+  const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }))
+  await app.listen(configService.get('PORT') || 5000);
 }
+
 module.exports = bootstrap;
